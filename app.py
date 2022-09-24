09590d912b02f9395de4975268db698a458bf6e7
YadavAnish123 <anishkumar3787230@gmail.com>
2022-09-24 16:55:17 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,176 @@
+from flask import Flask, render_template, Response, request
+import cv2
+import datetime, time
+import os, sys
+import numpy as np
+from threading import Thread
+## csv code
+import pandas as pd
+read_file = pd.read_excel ("C:\\Users\\anish\\Desktop\\IBM2\\Book.xlsx")
+read_file.to_csv ("Test.csv", 
+                  index = None,
+                  header=True)  
+df = pd.DataFrame(pd.read_csv("Test.csv"))  
+df.to_csv("Test.csv")
+df=df.set_index("Food Name")
+def Nutrients(Name):
+    name=Name
+    return(df.loc[(name),:])
+## 
+global capture,rec_frame, grey, switch, neg, face, rec, out,p,d
+capture=0
+grey=0
+neg=0
+face=0
+switch=1
+rec=0
+# ML
+import keras
+import cv2
+import tensorflow as tf
+import PIL.Image
+#from tensorflow.keras.utils import to_categorical
+#from tensorflow.keras.preprocessing.image import load_img, img_to_array
+from keras_preprocessing.image import load_img,img_to_array
+#from tensorflow.python.keras.preprocessing.image import ImageDataGenerator
+from keras.preprocessing.image import ImageDataGenerator
+
+#import tensorflow.compat.v2 as tf
+model = keras.models.load_model('C:\\Users\\anish\\Desktop\\IBM2\\foodModel.model')
+import numpy as np
+
+
+def image(path):
+  image=load_img(path,target_size=(40,40))
+  image=img_to_array(image) 
+  image=image/255.0
+  prediction_image=np.array(image)
+  prediction_image= np.expand_dims(image, axis=0)
+  return prediction_image
+ 
+#make shots directory to save pics
+try:
+    os.mkdir('./shots')
+except OSError as error:
+    pass
+ 
+#instatiate flask app  
+app = Flask(__name__, template_folder='./templates')
+ 
+camera = cv2.VideoCapture(0)
+# def Path(d):
+#     a=d
+#     return a
+
+ 
+def gen_frames():  # generate frame by frame from camera
+    global out, capture,rec_frame,d
+    while True:
+        success, frame = camera.read()
+        if success: 
+            if(capture):
+                capture=0
+                now = datetime.datetime.now()
+                p = os.path.sep.join(['shots', "shot_{}.png".format(str(now).replace(":",''))])
+                #d=("C:\\Users\\anish\\Desktop\\IBM2\\"+p)
+                cv2.imwrite(p, frame)
+                d=p
+                
+                 
+            
+                
+            try:
+                ret, buffer = cv2.imencode('.jpg', cv2.flip(frame,1))
+                frame = buffer.tobytes()
+                yield (b'--frame\r\n'
+                       b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+            except Exception as e:
+                pass
+                
+        else:
+            pass
+ 
+
+ 
+ 
+
+ 
+@app.route('/')
+def index():
+    return render_template('index.html')
+    
+@app.route('/uplod') 
+def uplod():
+    return render_template('index.html')  
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+ 
+ 
+@app.route('/requests',methods=['POST','GET'])
+def tasks():
+    global switch,camera
+    if request.method == 'POST':
+        if request.form.get('click') == 'Capture':
+            global capture
+            capture=1
+
+        elif request.form.get('detect') == 'Detect':
+            # prediction = model.predict([image("C:\\Users\\anish\\Desktop\\IBM2\\download.jfif")])
+            
+            path = os.getcwd()
+            print(d)
+            p=os.path.join(path, "", d )
+            print(p)
+            ##
+            train_dir = 'C:\\Users\\anish\\Desktop\\IBM2\\ibmFoodDataSet'
+            Name=[]
+            for file in os.listdir(train_dir):
+                Name+=[file]
+            print(Name)
+            print(len(Name))
+            N=[]
+            for i in range(len(Name)):
+                N+=[i]
+            mapping=dict(zip(Name,N)) 
+            reverse_mapping=dict(zip(N,Name)) 
+            def mapper(value):
+                return reverse_mapping[value]
+            import numpy as np
+            prediction=model.predict(image(p))
+            value=np.argmax(prediction)
+            move_name=mapper(value)
+            print("Prediction is {}.".format(move_name))
+            ##
+            # prediction = model.predict([image(p)])  
+             
+
+            # name=(CATEGORIES[prediction.argmax()])
+            # Product_name=name
+            data=Nutrients(move_name)
+            return render_template('Predect.html',name=move_name,data=data)
+         
+        elif  request.form.get('stop') == 'Stop/Start':
+            if(switch==1):
+                switch=0
+                camera.release()
+                cv2.destroyAllWindows()
+                
+            else:
+                camera = cv2.VideoCapture(0)
+                switch=1
+         
+                          
+                
+    elif request.method=='GET':
+        return render_template('index.html')
+    return render_template('index.html')
+ 
+ 
+if __name__ == '__main__':
+    app.run()
+    
+camera.release()
+cv2.destroyAllWindows()
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+def Detector():
+#     print('Hii this is anish')
+#     prediction = model.predict([image('C:\\Users\\anish\\Desktop\\IBM2\\download.jfif')])
+
+#     predected=(CATEGORIES[prediction.argmax()])
+#     return render_template('Predect.html',data=data)
